506dbda09d8c1ff9c19298a4079a93bf71322654
ivario123 <ivar.jonsson18@gmail.com>
2022-05-22 16:13:50 +0200

    moved docs again

--- docs/debug/deps/debug_print-07db7eefe12b855c.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/ivarj/Documents/Git/matrus/docs/debug/deps/debug_print-07db7eefe12b855c.rmeta: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/debug_print-1.0.0/src/lib.rs
-
-/mnt/c/Users/ivarj/Documents/Git/matrus/docs/debug/deps/debug_print-07db7eefe12b855c.d: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/debug_print-1.0.0/src/lib.rs
-
-/root/.cargo/registry/src/github.com-1ecc6299db9ec823/debug_print-1.0.0/src/lib.rs:
